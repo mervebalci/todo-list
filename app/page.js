691f35903d52043e010f3a1f6c1c60cd7a19d40e
Merve Balci <mervebalci090@gmail.com>
2023-11-19 22:19:12 -0400

    Activating the Add button with user input

--- app/page.js
+++ app/page.js
@@ -1,31 +1,45 @@
-import 'styles/styles.css'
-
-export function TodoList({ value }) {
-  return (
-    <li>
-      <label>
-        <input type="checkbox" />
-        <span>{value}</span>
-      </label>
-    </li>
-  )
-}
+'use client';
+import 'styles/styles.css';
+import { createRoot } from 'react-dom/client';
 
 export default function Home() {
+  function buttonClicked() {
+    const userInput = document.getElementById("addInput").value
+    
+    const newItem = <TodoItem value={userInput} />
+  
+    const container = document.getElementById('list');
+    const root = createRoot(container);
+    root.render(newItem)
+
+  }
+
   return (
     <main className="flex min-h-screen flex-col">
       <h1>TODO LIST</h1>
       <div id="addItem">
         <input id="addInput" type="text" placeholder="Add a new to do..." />
-        <button id="addButton">Add</button>
+        <button id="addButton" onClick={buttonClicked}>Add</button>
       </div>
       <div id="todoList">
         <ul id="list">
-          <TodoList value=" Cook dinner" />
-          <TodoList value=" Feed the cats" />
-          <TodoList value=" Water the plants" />
+
         </ul>
       </div>
     </main>
-  )
+  );
+}
+
+function TodoItem({ value }) {
+  console.log("hey");
+  console.log(value);
+
+  return (
+    <li>
+      <label>
+        <input type="checkbox" />
+        <span> {value}</span>
+      </label>
+    </li>
+  );
 }
